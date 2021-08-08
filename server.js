4761a1caefa2e2c5a384a888162525ac837a72f8
Muizz Ahmed <muizz.ahmed323@gmail.com>
2021-08-07 23:37:38 -0400

    updated server.js

--- server.js
+++ server.js
@@ -83,14 +83,9 @@ app.get('/data/summary/:days', (req, res) => {
 });
 
 
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
-  // Handle React routing, return all requests to React app
-  app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-}
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 
 app.listen(port, () => console.log(`Example app started on port ${port}`));
